b4fa760627c65a327aef3b3440b05c3dabfdf0f9
Sam Cox <sjamcox@gmail.com>
2021-01-14 20:40:24 -0800

    Add tests for root route

--- server/app.test.js
+++ server/app.test.js
@@ -2,8 +2,18 @@ const app = require('./app.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
-test('Root route should return 200 status code.', async (done) => {
-  const res = await request.get('/');
-  expect(res.status).toBe(200);
-  done();
+describe('Root route', () => {
+
+  it('should return 200 status code', async (done) => {
+    const res = await request.get('/');
+    expect(res.status).toBe(200);
+    done();
+  });
+
+  it('should send HTML in response', async (done) => {
+    const res = await request.get('/');
+    expect(res.type).toBe('text/html')
+    done();
+  });
+
 });
