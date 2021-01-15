dc637064d53cedb2728f39643bb6d75e30c87a04
Sam Cox <sjamcox@gmail.com>
2021-01-14 20:21:16 -0800

    Separate app logic and server logic for jest testing

--- server/index.js
+++ server/index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const app = express();
+const app = require('./app.js');
 const PORT = 5007;
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
