b707e8673a8029f2cd19390c50cb89e64a2f2ef8
Sam Cox <sjamcox@gmail.com>
2021-01-15 17:09:13 -0800

    Set up basic React app and container component

---
+++ client/src/components/MorePlacesSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MorePlacesSection = () => {
+  return (
+    <div>Loaded</div>
+  );
+};
+
+export default MorePlacesSection;
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MorePlacesSection from './components/MorePlacesSection';
+
+ReactDOM.render(<MorePlacesSection />, document.getElementById('root'));
