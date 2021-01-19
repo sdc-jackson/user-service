6d1f6aee8a70882da10c54fb73ab6156f0d60495
Sam Cox <sjamcox@gmail.com>
2021-01-18 19:41:33 -0800

    Update seeding script to run in batches
    - Seeding script was causing a socket hanging error
    - Batching user creation in groups of less than 100 seems to solve the issue

--- server/database/seed.js
+++ server/database/seed.js
@@ -2,42 +2,27 @@ require('dotenv').config();
 const axios = require('axios');
 const faker = require('faker');
 const AWS = require('./aws.js');
+const { mongoose, User } = require('./schema.js');
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .catch(err => console.error(err));
-
-const userSchema = new mongoose.Schema({
-  userId: Number,
-  name: String,
-  joinDate: String,
-  bio: String,
-  avatarUrl: String,
-  isSuperhost: Boolean,
-  identityVerified: Boolean,
-  languages: Array,
-  responseRate: Number,
-  responseTime: Number
-});
-
-const User = mongoose.model('User', userSchema);
+const languages = ['English', 'Spanish', 'French', 'Portuguese', 'German', 'Italian', 'Cambodian', 'Thai', 'Shyriiwook'];
 
 const generatePhoto = async () => {
-  const photo = await axios({
-    url: 'https://picsum.photos/50',
-    method: 'GET',
-    responseType: 'stream'
-  });
-  return photo;
+  try {
+    const photo = await axios({
+      url: 'https://picsum.photos/75',
+      method: 'GET',
+      responseType: 'stream'
+    });
+    return photo;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-const createUser = async (id) => {
+const seedUser = async (id) => {
   try {
     const photo = await generatePhoto();
-    const S3Url = await AWS.uploadPhotoToS3(photo)
+    const S3Url = await AWS.uploadPhotoToS3(photo);
     const user = new User({
       userId: id,
       name: faker.name.firstName(),
@@ -49,27 +34,39 @@ const createUser = async (id) => {
       languages: faker.random.arrayElements(languages, faker.random.number({ min: 0, max: 3 })),
       responseRate: faker.random.number({ min: 93, max: 100 }),
       responseTime: faker.random.number({ min: 10, max: 600 })
-    })
-    const saveData = await user.save()
-    return saveData
-  } catch(err) {
-    console.error(err)
+    });
+    const saveData = await user.save();
+    return saveData;
+  } catch (err) {
+    console.error(err);
   }
-}
+};
 
-const languages = ['English', 'Spanish', 'French', 'Portuguese', 'German', 'Italian', 'Cambodian', 'Thai', 'Shyriiwook'];
+const seedManyUsers = (start, number) => {
+  const ids = [];
+  // batchSize determines how many users get created simultaneously
+  // Use batchSize to not overload image service with too many requests at once
+  // Batches of 100+ requests known to cause socket error
+  let batchSize = 50;
 
-const seedManyUsers = async (start, number) => {
-  let promises = []
-  while (number > 0) {
-    promises.push(createUser(start))
-    start++
-    number--
+  while (number > 0 && batchSize > 0) {
+    ids.push(start);
+    start++;
+    number--;
+    batchSize--;
   }
-  return Promise.all(promises)
+  const promises = ids.map(id => seedUser(id));
+  Promise.all(promises)
     .then(() => {
-      console.log('Seeding complete')
+      console.log('Batch complete');
+      if (number > 0) {
+        seedManyUsers(start, number);
+      } else {
+        console.log('Seeding complete');
+        mongoose.disconnect();
+      }
     })
-}
+    .catch(() => console.error('Batch unsuccessful'));
+};
 
-seedManyUsers(100, 100)
+seedManyUsers(100, 100);
