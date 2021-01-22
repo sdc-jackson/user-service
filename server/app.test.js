d20a8c9f4ae22a30138beb937cad43c58eb178c8
Sam Cox <sjamcox@gmail.com>
2021-01-22 11:33:25 -0800

    Remove unnecessary test

--- server/app.test.js
+++ server/app.test.js
@@ -1,19 +1,3 @@
 const app = require('./app.js');
 const supertest = require('supertest');
 const request = supertest(app);
-
-describe('Root route', () => {
-
-  it('should return 200 status code', async (done) => {
-    const res = await request.get('/');
-    expect(res.status).toBe(200);
-    done();
-  });
-
-  it('should send HTML in response', async (done) => {
-    const res = await request.get('/');
-    expect(res.type).toBe('text/html');
-    done();
-  });
-
-});
