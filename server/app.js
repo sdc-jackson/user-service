5be508d4316b73a8f3f14ac205a36a06352869c2
Sam Cox <sjamcox@gmail.com>
2021-01-23 12:26:22 -0800

    Add GET /users/:userId route for fetching user data

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const { getUserById } = require('./database/helpers');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+app.get('/users/:userId', async (req, res) => {
+  try {
+    const user = await getUserById(req.params.userId);
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(500).send({ message: 'Server Error' });
+  }
 });
 
 module.exports = app;
