049d0e8a6d3db34aa20151fc7a371a89da535ca0
Sam Cox <sjamcox@gmail.com>
2021-01-27 00:25:52 -0800

    Create CohostList component

---
+++ client/src/components/CohostList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Cohost from './Cohost';
+import { query } from '../utils';
+
+const CohostListContainer = styled.div`
+  display: none;
+  .section-header {
+    font-size: 1.6rem;
+    margin: 0 0 8px 0;
+  }
+  @media (min-width: ${query.large}) {
+    display: block;
+  }
+`;
+
+const CohostList = ({ cohosts }) => {
+  const cohostList = cohosts.map((cohost, i) => <Cohost avatarUrl={cohost.avatarUrl} name={cohost.name} key={`${cohost.name}-${i}`} />);
+  return (
+    <CohostListContainer>
+      <p className='section-header'>Co-hosts</p>
+      { cohostList }
+    </CohostListContainer>
+  );
+};
+
+CohostList.propTypes = {
+  cohosts: PropTypes.array.isRequired
+};
+
+export default CohostList;
