765ed581cacfc282d4abdf92425309ede1ca476e
Sam Cox <sjamcox@gmail.com>
2021-01-27 09:56:57 -0800

    Add HostCommunications to HostSection

--- client/src/components/HostSection.jsx
+++ client/src/components/HostSection.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import HostId from './HostId';
 import HostStats from './HostStats';
 import HostDescription from './HostDescription';
+import HostCommunications from './HostCommunications';
 import styled from 'styled-components';
 import { data } from '../../../testdata';
 import { query } from '../utils';
@@ -33,6 +34,11 @@ const HostSection = () => {
         duringYourStay={`My interaction with guests will be minimal. Although I may bump into you occasionally, we'll give you as much privacy as we can. Since I live in another house on the property, I'm always around if you need anything. Texting is best, but you can call or knock on my door if it's urgent.`}
         isSuperhost={true || host.isSuperhost}
         name={host.name}
+      />
+      <HostCommunications
+        languages={host.languages}
+        responseRate={host.responseRate}
+        responseTime={host.responseTime}
       />
     </HostSectionContainer>
   );
