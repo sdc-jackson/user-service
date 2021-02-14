b81d4cbe07f257b71fb36fcfe49348535df7372d
Sam Cox <sjamcox@gmail.com>
2021-02-13 16:28:58 -0800

    Add InnerSectionContainer for correct margins on page body

--- client/src/components/HostSection.jsx
+++ client/src/components/HostSection.jsx
@@ -11,13 +11,19 @@ import { query } from '../utils';
 const HostSectionContainer = styled.section`
   font-family: "Roboto";
   padding: 32px 24px;
-  width: 100%;
+  width: auto;
   @media (min-width: ${query.medium}) {
     padding: 48px 40px;
   }
   @media (min-width: ${query.large}) {
     padding: 48px 80px;
   }
+`;
+
+const SectionInnerContainer = styled.div`
+  margin: 0 auto;
+  max-width: 1128px;
+  width: 100%;
 `;
 
 const TwoColumn = styled.div`
@@ -65,31 +71,34 @@ const HostSection = () => {
 
   return (
     <HostSectionContainer>
-      <HostId host={host} />
-      <TwoColumn>
-        <div className='col-one'>
-          <HostStats
-            isSuperhost={host.isSuperhost}
-            isVerified={host.identityVerified}
-            reviews={17} // Needs data from another service
-          />
-          <HostDescription
-            bio={host.bio}
-            cohosts={[{name: host.name, avatarUrl: host.avatarUrl }]}
-            duringYourStay={`My interaction with guests will be minimal. Although I may bump into you occasionally, we'll give you as much privacy as we can. Since I live in another house on the property, I'm always around if you need anything. Texting is best, but you can call or knock on my door if it's urgent.`} // Needs data from another service
-            isSuperhost={host.isSuperhost}
-            name={host.name}
-          />
-        </div>
-        <div className='col-two'>
-          <HostCommunications
-            languages={host.languages}
-            responseRate={host.responseRate}
-            responseTime={host.responseTime}
-          />
-          <Security />
-        </div>
-      </TwoColumn>
+      <SectionInnerContainer>
+
+        <HostId host={host} />
+        <TwoColumn>
+          <div className='col-one'>
+            <HostStats
+              isSuperhost={host.isSuperhost}
+              isVerified={host.identityVerified}
+              reviews={17} // Needs data from another service
+            />
+            <HostDescription
+              bio={host.bio}
+              cohosts={[{name: host.name, avatarUrl: host.avatarUrl }]}
+              duringYourStay={`My interaction with guests will be minimal. Although I may bump into you occasionally, we'll give you as much privacy as we can. Since I live in another house on the property, I'm always around if you need anything. Texting is best, but you can call or knock on my door if it's urgent.`} // Needs data from another service
+              isSuperhost={host.isSuperhost}
+              name={host.name}
+            />
+          </div>
+          <div className='col-two'>
+            <HostCommunications
+              languages={host.languages}
+              responseRate={host.responseRate}
+              responseTime={host.responseTime}
+            />
+            <Security />
+          </div>
+        </TwoColumn>
+      </SectionInnerContainer>
     </HostSectionContainer>
   );
 };
