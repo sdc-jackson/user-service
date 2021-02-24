9a30968fcc4a6c5adf88d65a93425c289f6850b1
Sam Cox <sjamcox@gmail.com>
2021-02-23 22:29:37 -0800

    WIP

--- server/database/schema.js
+++ server/database/schema.js
@@ -5,7 +5,8 @@ if (process.env.NODE_ENV !== 'test') {
   mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }, () => { console.log('connected to FEC'); })
+  })
+    .then(() => console.log('connected to fec'))
     .catch(err => console.error(err));
 }
 
