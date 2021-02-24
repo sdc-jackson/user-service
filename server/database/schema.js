2b29a352c9fa249383a67c4cd986aaeef38e717c
Sam Cox <sjamcox@gmail.com>
2021-02-23 22:20:56 -0800

    Add db connect log

--- server/database/schema.js
+++ server/database/schema.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'test') {
   mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
+  }, () => { console.log('connected to FEC'); })
     .catch(err => console.error(err));
 }
 
